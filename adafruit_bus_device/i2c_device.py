bdc4fd7bedf1db6bdacd53ac6c0fa62a998f91b3
Scott Shawcroft <scott.shawcroft@gmail.com>
2016-12-01 18:34:46 -0800

    First version of I2CDevice

---
+++ adafruit_bus_device/i2c_device.py
@@ -0,0 +1,90 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2016 Scott Shawcroft for Adafruit Industries
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+class I2CDevice:
+    """
+    Represents a single I2C device and manages locking the bus and the device
+    address.
+
+    :param I2C i2c: The I2C bus the device is on
+    :param int device_address: The 7 bit device address
+
+    Example::
+
+        import nativeio
+        from board import *
+        from adafruit_bus_device.i2c_device import I2CDevice
+
+        with nativeio.I2C(SCL, SDA) as i2c:
+            device = I2CDevice(i2c, 0x70)
+            bytes_read = bytearray(4)
+            with device:
+                device.readfrom_into(bytes_read)
+            # A second transaction
+            with device:
+                device.writeto(bytes_read)
+    """
+    def __init__(self, i2c, device_address):
+        self.i2c = i2c
+        self.device_address = device_address
+
+    def readfrom_into(self, buf, **kwargs):
+        """
+        Read into ``buf`` from the device. The number of bytes read will be the
+        length of ``buf``.
+
+        If ``start`` or ``end`` is provided, then the buffer will be sliced
+        as if ``buf[start:end]``. This will not cause an allocation like
+        ``buf[start:end]`` will so it saves memory.
+
+             :param int address: 7-bit device address
+             :param bytearray buffer: buffer to write into
+             :param int start: Index to start writing at
+             :param int end: Index to write up to but not include
+        """
+        self.i2c.readfrom_into(self.device_address, buf, **kwargs)
+
+    def writeto(self, buf, **kwargs):
+        """
+        Write the bytes from ``buffer`` to the device. Transmits a stop bit if
+        ``stop`` is set.
+
+        If ``start`` or ``end`` is provided, then the buffer will be sliced
+        as if ``buffer[start:end]``. This will not cause an allocation like
+        ``buffer[start:end]`` will so it saves memory.
+
+          :param bytearray buffer: buffer containing the bytes to write
+          :param int start: Index to start writing from
+          :param int end: Index to read up to but not include
+          :param bool stop: If true, output an I2C stop condition after the
+                            buffer is written
+        """
+        self.i2c.writeto(self.device_address, buf, **kwargs)
+
+    def __enter__(self):
+        while not self.i2c.try_lock():
+            pass
+        return self
+
+    def __exit__(self, *exc):
+        self.i2c.unlock()
+        return False
