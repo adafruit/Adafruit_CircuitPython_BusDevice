909fcf7267adae37a5ce34d2c484fa68d3f6381f
Dan Halbert <halbert@halwitz.org>
2022-02-11 15:21:08 -0500

    blinka substitutes for mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ extensions = [
 # Uncomment the below if you use native CircuitPython modules such as
 # digitalio, micropython and busio. List the modules you use. Without it, the
 # autodoc module docs will fail to generate with a warning.
-autodoc_mock_imports = ["busio", "digitalio", "circuitpython_typing"]
+# autodoc_mock_imports = []
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
