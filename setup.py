2dee41e0f52479a2edb6e09b8049ef722ca92bcf
Dan Halbert <halbert@halwitz.org>
2022-02-15 19:24:28 -0500

    require circuitpython_typing

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     # Author details
     author="Adafruit Industries",
     author_email="circuitpython@adafruit.com",
-    install_requires=["Adafruit-Blinka"],
+    install_requires=["Adafruit-Blinka>=6.20.4", "adafruit-circuitpython-typing"],
     # Choose your license
     license="MIT",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
